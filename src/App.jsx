521bce254f440db228d435ed60add9fd3b72fa51
Jayesh421 <wanijayesh0@gmail.com>
2024-02-10 23:22:36 +0530

    home

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 10px;
+  min-height: calc(100vh - 50px);
+  position: relative;
+  margin-bottom: 50px;
+`;
+
+export default function App() {
+  return (
+    <Container>
+    </Container>
+  );
+}
