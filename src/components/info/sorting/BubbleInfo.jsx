885bd45ea0ca03cc9bb3eaef0f7f0fcf018ca187
Jayesh421 <wanijayesh0@gmail.com>
2024-03-30 00:38:32 +0530

    bubble

--- src/components/info/sorting/BubbleInfo.jsx
+++ src/components/info/sorting/BubbleInfo.jsx
@@ -140,23 +140,15 @@ export function BubbleInfo() {
             >
               <pre>
                 {`
-# Python program for implementation of Bubble Sort
 def bubbleSort(arr):
 n = len(arr)
  
-# Traverse through all array elements
 for i in range(n):
  
-    # Last i elements are already in place
     for j in range(0, n-i-1):
- 
-        # traverse the array from 0 to n-i-1
-        # Swap if the element found is greater
-        # than the next element
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
  
-# Driver code to test above
 if __name__ == "__main__":
   arr = [64, 34, 25, 12, 22, 11, 90]
  
@@ -183,8 +175,7 @@ if __name__ == "__main__":
             >
               <pre>
                 {
-`// C program for implementation of Bubble sort
-#include <stdio.h>
+`#include <stdio.h>
 
 void swap(int* xp, int* yp)
 {
