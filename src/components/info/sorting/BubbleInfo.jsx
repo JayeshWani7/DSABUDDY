7768f7ce7ea369df56bebb5b9e478d19bb8e0cc3
Jayesh421 <wanijayesh0@gmail.com>
2024-03-30 19:40:56 +0530

    bubble sort

--- src/components/info/sorting/BubbleInfo.jsx
+++ src/components/info/sorting/BubbleInfo.jsx
@@ -117,9 +117,8 @@ export function BubbleInfo() {
               variant="scrollable"
             >
               <Tab label="Python" {...0} />
-              <Tab label="C" {...1} />
-              <Tab label="C++" {...2} />
-              <Tab label="Java" {...3} />
+              <Tab label="C++" {...1} />
+              <Tab label="Java" {...2} />
             </Tabs>
           </Box>
           <TabPanel
@@ -157,70 +156,7 @@ if __name__ == "__main__":
               </pre>
             </div>
           </TabPanel>
-          <TabPanel
-            value={value}
-            index={1}
-            style={{
-              backgroundClip: "border-box",
-              backgroundColor: "rgb(56,59,64)",
-            }}
-          >
-            <div
-              style={{
-                backgroundClip: "border-box",
-                backgroundColor: "rgb(56,59,64)",
-                color: " rgb(211, 211, 211)",
-                padding: "10px",
-              }}
-            >
-              <pre>
-                {
-`#include <stdio.h>
-
-void swap(int* xp, int* yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-	int i, j;
-	for (i = 0; i < n - 1; i++)
-
-		// Last i elements are already in place
-		for (j = 0; j < n - i - 1; j++)
-			if (arr[j] > arr[j + 1])
-				swap(&arr[j], &arr[j + 1]);
-}
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
-
-// Driver program to test above functions
-int main()
-{
-	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	bubbleSort(arr, n);
-	printf("Sorted array: \n");
-	printArray(arr, n);
-	return 0;
-}
-`
-                }
-              </pre>
-            </div>
-          </TabPanel>
-          <TabPanel value={value} index={2}>
+          <TabPanel value={value} index={1}>
             <div
               style={{
                 backgroundClip: "border-box",
@@ -274,7 +210,7 @@ int main()
                 </pre>
             </div>
           </TabPanel>
-          <TabPanel value={value} index={3}>
+          <TabPanel value={value} index={2}>
           <div
               style={{
                 backgroundClip: "border-box",
