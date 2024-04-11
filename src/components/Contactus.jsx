66a64e22926122022c7628f7f6a84a010dc610ce
Jayesh421 <wanijayesh0@gmail.com>
2024-04-12 00:09:07 +0530

    Footer

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -11,7 +11,6 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
     fontFamily: "Nunito",
     flexDirection: "column",
-    
   },
   TitleSec: {
     fontFamily: "Nunito",
@@ -62,74 +61,103 @@ const useStyles = makeStyles((theme) => ({
     color: "#3f51b5",
     fontSize: "4rem",
   },
+  footer: {
+    background: "#fff",
+    alignItems: "center",
+    padding: "20px",
+    textAlign: "center",
+    marginTop: "20px",
+    width: "100%",
+    marginBottom: "0"
+  },
+  footerLink: {
+    textDecoration: "none",
+    color: "#3f51b5"
+  },
 }));
 
-export default function () {
+const Footer = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root} id="ContactUs">
-      <div className={classes.TitleSec}>
-        <p>Get in Touch</p>
-        <h1 className={classes.colorText}>CONTACT</h1>
-      </div>
-      <div className={classes.bodySec}>
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={5}>
-            <div className={classes.contentSec}>
-              <Card className={classes.InfoCard}>
-                <Avatar className={classes.colorBg}>
-                  <Phone />
-                </Avatar>
-                <p className={classes.Infotext}>+91 9307695937</p>
-              </Card>
-              <Card className={classes.InfoCard}>
-                <Avatar className={classes.colorBg}>
-                  <Email />
-                </Avatar>
-                <p className={classes.Infotext}>dsabuddy@gmail.com</p>
-              </Card>
-              <Card className={classes.InfoCard}>
-                <Avatar className={classes.colorBg}>
-                  <Home />
-                </Avatar>
-                <p className={classes.Infotext}>Kurla, Mumbai</p>
-              </Card>
-              <Card className={classes.InfoCard}>
-                <Avatar className={classes.colorBg}>
-                  <LocationCity />
-                </Avatar>
-                <p className={classes.Infotext}>Don Bosco Institute of Technology</p>
-              </Card>
-            </div>
+    <footer className={classes.footer}>
+      <p>All rights reserved © DSABUDDY 2024 | Designed by 
+      <a href="https://www.linkedin.com/in/jayesh-wani/" target="_blank" className={classes.footerLink}> Jayesh Wani</a>, 
+      <a href="https://www.linkedin.com/in/sanjay-yadav/" target="_blank" className={classes.footerLink}> Sanjay Yadav</a>, 
+      <a href="https://www.linkedin.com/in/nibin-baby/" target="_blank" className={classes.footerLink}> Nibin Baby</a>
+      </p>
+    </footer>
+  );
+};
+
+export default function App() {
+  const classes = useStyles();
+  return (
+    <div>
+      <div className={classes.root} id="ContactUs">
+        <div className={classes.TitleSec}>
+          <p>Get in Touch</p>
+          <h1 className={classes.colorText}>CONTACT</h1>
+        </div>
+        <div className={classes.bodySec}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={5}>
+              <div className={classes.contentSec}>
+                <Card className={classes.InfoCard}>
+                  <Avatar className={classes.colorBg}>
+                    <Phone />
+                  </Avatar>
+                  <p className={classes.Infotext}>+91 9307695937</p>
+                </Card>
+                <Card className={classes.InfoCard}>
+                  <Avatar className={classes.colorBg}>
+                    <Email />
+                  </Avatar>
+                  <p className={classes.Infotext}>dsabuddy@gmail.com</p>
+                </Card>
+                <Card className={classes.InfoCard}>
+                  <Avatar className={classes.colorBg}>
+                    <Home />
+                  </Avatar>
+                  <p className={classes.Infotext}>Kurla, Mumbai</p>
+                </Card>
+                <Card className={classes.InfoCard}>
+                  <Avatar className={classes.colorBg}>
+                    <LocationCity />
+                  </Avatar>
+                  <p className={classes.Infotext}>Don Bosco Institute of Technology</p>
+                </Card>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <div className={classes.contentSec}>
+                <div className={classes.TextCard}>
+                  <p>Your name</p>
+                  <TextField id="outlined-basic" variant="outlined" />
+                </div>
+                <div className={classes.TextCard}>
+                  <p>Your Email</p>
+                  <TextField id="outlined-basic" variant="outlined" />
+                </div>
+                <div className={classes.TextCard}>
+                  <p>Your message</p>
+                  <TextField
+                    id="standard-basic"
+                    variant="outlined"
+                    multiline
+                    minrows={4}
+                  />
+                </div>
+                <div className={classes.TextCard}>
+                  <Button variant="contained" className={classes.submitbtn}>
+                    Submit
+                  </Button>
+                </div>
+              </div>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={7}>
-            <div className={classes.contentSec}>
-              <div className={classes.TextCard}>
-                <p>Your name</p>
-                <TextField id="outlined-basic" variant="outlined" />
-              </div>
-              <div className={classes.TextCard}>
-                <p>Your Email</p>
-                <TextField id="outlined-basic" variant="outlined" />
-              </div>
-              <div className={classes.TextCard}>
-                <p>Your message</p>
-                <TextField
-                  id="standard-basic"
-                  variant="outlined"
-                  multiline
-                  minrows={4}
-                />
-              </div>
-              <div className={classes.TextCard}>
-                <Button variant="contained" className={classes.submitbtn}>
-                  Submit
-                </Button>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
